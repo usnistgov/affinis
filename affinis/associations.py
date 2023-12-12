71a35c1ede10b7c746b384838e2523c1ba7d31bb
Rachael T. Sexton <tbs4@nist.gov>
2023-12-12 12:32:17 -0500

    working dvc random graph pipe

--- affinis/associations.py
+++ affinis/associations.py
@@ -1,5 +1,6 @@
 import numpy as np
 from scipy.sparse.csgraph import minimum_spanning_tree
+from scipy.sparse import coo_array, issparse
 from .utils import _sq, _outer
 
 """
@@ -18,10 +19,14 @@ of meaning that allow for this, where we can.
 TODO: define type signatures by array shapes
 TODO: dispatch on dataframes, static-frames, etc.
 """
+def _gram(X1,X2):
+    grammian = X1.T@X2
+    return grammian.toarray() if issparse(grammian) else grammian
 
-def coocur_prob(X, pseudocts=0.0):
+def coocur_prob(X, pseudocts=0.5):
     """probability of a co-ocurrence per-observation"""
-    cts = X.T @ X + pseudocts
+
+    cts = _gram(X, X) + pseudocts
     tot = X.shape[0] + 2 * pseudocts
     return cts / tot
 
@@ -31,9 +36,15 @@ def coocur_prob(X, pseudocts=0.0):
 #     tot = X.sum(axis=0)+2
 #     return cts/tot
 
+def _not(X):
+    if issparse(X):
+        nX = X<coo_array(np.ones(X.shape))
+    else: 
+        nX = 1 - X
+    return nX
 
 def _marginal_cts(X):
-    return X.sum(axis=0), (1 - X).sum(axis=0)
+    return X.sum(axis=0), _not(X).sum(axis=0)
 
 
 def _marginal_prob(X):
@@ -41,14 +52,15 @@ def _marginal_prob(X):
 
 
 def _contingency_cts(X):
-    both = _sq(X.T @ X)
-    one = _sq(X.T @ (1 - X))
-    other = _sq((1 - X).T @ X)
-    neither = _sq((1 - X).T @ (1 - X))
+    nX = _not(X)
+    both = _sq(_gram(X,X))
+    one = _sq(_gram(X,nX))
+    other = _sq(_gram(nX,X))
+    neither = _sq(_gram(nX,nX))
     return neither, one, other, both
 
 
-def _contingency_prob(X, pseudocts=0.0):
+def _contingency_prob(X, pseudocts=0.5):
     cts = np.vstack(_contingency_cts(X)) + pseudocts
     return [ct for ct in cts / (X.shape[0] + pseudocts * 2)]
 
@@ -64,7 +76,7 @@ def _binary_contingency(X):
     return neither, one, other, both
 
 
-def odds_ratio(X, pseudocts=0.0):
+def odds_ratio(X, pseudocts=0.5):
     """Ratio of the odds of a true pos/neg to false pos/neg
     
     For associations, we replace pos/neg and true/false with 
@@ -74,7 +86,7 @@ def odds_ratio(X, pseudocts=0.0):
     return _sq(a * d / (b * c)) + np.eye(X.shape[1])
 
 
-def mutual_information(X, pseudocts=0.0):
+def mutual_information(X, pseudocts=0.5):
     """Mutual Information over binary random variables
     
     For use in e.g. Chow-Liu Trees
@@ -98,7 +110,7 @@ def mutual_information(X, pseudocts=0.0):
     # return sq(entropy(Pxy, PxPy))
 
 
-def chow_liu(X, pseudocts=0.0):
+def chow_liu(X, pseudocts=0.5):
     """Chow-Liu Tree on the features of a (binary) design matrix
     
     computes mutual information over all pairs of features, and returns
@@ -114,7 +126,7 @@ def chow_liu(X, pseudocts=0.0):
     )
 
 
-def yule_y(X, pseudocts=0.0):
+def yule_y(X, pseudocts=0.5):
     """a.k.a. Coefficient of Colligation. 
     
     mobius transform of the Odds Ratio to the range [-1,1]
@@ -125,7 +137,7 @@ def yule_y(X, pseudocts=0.0):
     # return ((ad:=np.sqrt(both*neither))-(bc:=np.sqrt(one*other)))/(ad+bc)
 
 
-def yule_q(X, pseudocts=0.0):
+def yule_q(X, pseudocts=0.5):
     """a.k.a. Goodman & Kruskal's gamma for 2x2. 
 
     mobius transform of the Odds Ratio to the range [-1,1]
@@ -134,7 +146,7 @@ def yule_q(X, pseudocts=0.0):
     return _sq(((ad := a * d) - (bc := b * c)) / (ad + bc))
 
 
-def ochiai(X, pseudocts=0.0):
+def ochiai(X, pseudocts=0.5):
     """AKA cosine similarity on binary sets
 
     This code illustrates the idea that we can interpret it as conditional probability:
@@ -146,12 +158,12 @@ def ochiai(X, pseudocts=0.0):
     laplace/additive pseudocounts on each bernoulli(-ish) "co-occurrence variable".
     """
     # I = np.eye(X.shape[-1])
-    co_occurs = _sq(X.T @ X) + pseudocts
+    co_occurs = _sq(_gram(X,X)) + pseudocts
     exposures = X.sum(axis=0)
     pseudo_exposure = np.sqrt(_outer(np.multiply, exposures)) + 2 * pseudocts
     return _sq(co_occurs) / pseudo_exposure + np.eye(X.shape[1])
 
-def binary_cosine_similarity(X, pseudocts=0.):
+def binary_cosine_similarity(X, pseudocts=0.5):
     """alias of `ochiai(X), provided for user convenience"""
     return ochiai(X, pseudocts=pseudocts)
 
@@ -162,7 +174,37 @@ def resource_project(X):
     Really just a step of sinkhorn-knopp on the bipartite adjacency
     https://doc.rero.ch/record/8503/files/zhang_bnp.pdf
     """
-    P = ((X.T) / (X.sum(axis=1))) @ ((X) / (X.sum(axis=0)))
+    P = _gram(((X.T) / (X.sum(axis=1))).T, ((X) / (X.sum(axis=0))))
     return np.maximum(P, P.T)
 
 
+def spanning_forests_edge_prob(X, prior=ochiai, pseudocts=0.5):
+    """Forest for the Trees
+    
+    """
+    est_dists = -np.log(prior(X, pseudocts=pseudocts))
+
+    def get_mask(e_pmf,idx):
+        return coo_array(_sq(e_pmf)*np.multiply.outer(idx,idx))
+    
+    # def unroll_node_obs(X): 
+    #     trirow, tricol = np.triu_indices(n=X.shape[1],k=1)
+    #     return np.einsum('ij,ik->ijk', X, X)[:,trirow, tricol]
+
+    # project into edge-space, only retaining MST activations 
+    N_obs = X.toarray() if issparse(X) else X
+    E_obs=coo_array([_sq(minimum_spanning_tree(
+            get_mask(_sq(est_dists),i)
+        ).todense()>0) for i in N_obs
+    ])
+    # co_prob = coocur_prob(X,pseudocts=pseudocts)
+    # e_prob = (E_obs.sum(axis=0)+pseudocts)*_sq(co_prob)/_sq(_gram(X,X)+1)
+
+    # (E_obs.sum(axis=0)+0.5)*_sq(coocur_prob(X, pseudocts=0.5))/_sq(X.T@X+1)
+    e_prob = (E_obs.sum(axis=0)+pseudocts)/_sq(_gram(X,X)+2*pseudocts)
+    return _sq(e_prob)
+
+def spanning_forests_edge_rate(X, prior=ochiai, pseudocts=0.5):
+    e_prob = spanning_forests_edge_prob(X, prior=prior, pseudocts=pseudocts)
+    e_rate = X.shape[0]*_sq(e_prob)*_sq(coocur_prob(X, pseudocts=pseudocts))
+    return _sq(e_rate)
